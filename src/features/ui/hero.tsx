851a9e4911babea5376ce3394381014ccf0ec36e
Frank-InsAdv <fmennella@iaa.net.au>
2025-08-15 13:40:08 +1000

    Update hero.tsx

--- src/features/ui/hero.tsx
+++ src/features/ui/hero.tsx
@@ -14,7 +14,9 @@ export const Hero: FC<HeroProps> = (props) => {
           <h1 className="text-4xl font-bold flex gap-2 items-center">
             {props.title}
           </h1>
-          <p className="text-muted-foreground max-w-xl">{props.description}</p>
+           <p className="text-muted-foreground max-w-xl whitespace-pre-line">
+            {props.description}
+          </p>
         </div>
         <div className="grid grid-cols-3 gap-2">{props.children}</div>
       </div>
