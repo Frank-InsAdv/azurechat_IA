0530d6f3010850fb52b686722c0dd4b4ce3e7a6c
Frank-InsAdv <fmennella@iaa.net.au>
2025-08-15 13:27:30 +1000

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,6 +1,6 @@
 export const AI_NAME = "IA GPT";
-export const AI_DESCRIPTION = `Welcome to IA GPT!` + '\n' +
-  `An exclusive deployment of ChatGPT that is securely hosted within our internal network to ensure the highest level of data privacy and security, with all the power of ChatGPT.`;
+export const AI_DESCRIPTION = `Welcome to IA GPT!
+  An exclusive deployment of ChatGPT that is securely hosted within our internal network to ensure the highest level of data privacy and security, with all the power of ChatGPT.`;
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
